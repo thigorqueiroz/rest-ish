2a6e9bff461a2cb2a752c4f0b89fdc7a8a805e41
thigorqueiroz <thigorqueiroz@gmail.com>
2021-08-02 17:29:53 -0300

    WIP

--- server_test.go
+++ server_test.go
@@ -78,6 +78,19 @@ func TestStoreWins(t *testing.T) {
 	})
 }
 
+func TestLeague(t *testing.T) {
+	store := StubPlayerStore{}
+	server := &PlayerServer{&store}
+
+	t.Run("it returns 200 on /league", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/league", nil)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+		assertStatus(t, response.Code, http.StatusOK)
+	})
+}
+
 func assertResponseBody(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {
